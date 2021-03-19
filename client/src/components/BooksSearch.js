ddaefc9bef0ff2cf1c63283d189eff18f6905519
Ryan Conran <ryan.conran2@gmail.com>
2021-03-18 18:23:13 -0600

    Fixed API call

--- client/src/components/BooksSearch.js
+++ client/src/components/BooksSearch.js
@@ -1,56 +1,75 @@
-import React, { useState } from 'react';
-import API from '../utils/API';
+import React, { useState } from "react";
+import API from "../utils/API";
 
 function BooksSearch(props) {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [books, setBooks] = useState([]); 
-    const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState(null);
 
-    const onBookSearch = async () => {
-        try {
-            const result = await API.search(searchTerm);
-            setBooks(result.data.items);
-        } catch(ex) {
-            setError(ex);
-        }
+  const onBookSearch = async () => {
+    try {
+      const result = await API.search(searchTerm);
+      setBooks(result.data.items);
+      console.log(result);
+    } catch (ex) {
+      setError(ex);
     }
+  };
 
-    return (
+  return (
+    <>
+      {!books ? (
+        "We did not find that book!"
+      ) : (
         <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Input a Search Term"
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button onClick={onBookSearch}>Search</button>
+          </div>
+          {error && <div style={{ color: "red" }}>{error}</div>}
+          {books.length > 0 && (
             <div>
-                <input type="text" placeholder="Input a Search Term" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} /> 
-                <button onClick={onBookSearch}>Search</button>
-            </div>  
-            {error && <div style={{color:"red"}}>{error}</div>}          
-            {books.length > 0 && <div>
-                <p>Results</p>
-                <div>
-                    {books.map(book => (
-                        <div key={book.id}> 
-                            <div>
-                                <img src={book.volumeInfo.imageLinks.thumbnail} />
-                            </div>
-                            <div>
-                                <label>Title:</label>
-                                <span>{book.volumeInfo.title}</span>
-                            </div>
-                            <div>
-                                <label>Authors:</label>
-                                <span>{book.volumeInfo.authors && book.volumeInfo.authors.join(",")}</span>
-                            </div>
-                            <div>
-                                <label>Description:</label>
-                                <span>{book.volumeInfo.description}</span>
-                            </div>  
-                            <div>
-                                <span><a href={book.volumeInfo.infoLink}>Link</a></span>
-                            </div>                                                                                        
-                        </div>
-                    ))}
-                </div>
-            </div>}
+              <p>Results</p>
+              <div>
+                {books.map((book) => (
+                  <div key={book.id}>
+                    <div>
+                      <img src={book.volumeInfo.imageLinks.thumbnail} />
+                    </div>
+                    <div>
+                      <label>Title:</label>
+                      <span>{book.volumeInfo.title}</span>
+                    </div>
+                    <div>
+                      <label>Authors:</label>
+                      <span>
+                        {book.volumeInfo.authors &&
+                          book.volumeInfo.authors.join(",")}
+                      </span>
+                    </div>
+                    <div>
+                      <label>Description:</label>
+                      <span>{book.volumeInfo.description}</span>
+                    </div>
+                    <div>
+                      <span>
+                        <a href={book.volumeInfo.infoLink}>Link</a>
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-    )
+      )}
+    </>
+  );
 }
 
 export default BooksSearch;
